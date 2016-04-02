7ccf34347677661ff3be113488b7c85959243cd6
swordfishh <linrj@uci.edu>
2016-04-01 20:01:11 -0700

    email phone carrier information update

--- Log.py
+++ Log.py
@@ -1,4 +1,5 @@
 class Log(object):
+	# check README file for list of emails corresponding to various cell phone carriers
 	to_addr = '' #receiver (email or phone number email address)
 	from_addr = '' # sender (email)
 	authentication = '' # password for sender email
